d6815d8d53fface789151f31968f040508257540
Kevin Chen <kevinmichaelchen@gmail.com>
2019-06-02 20:46:27 -0400

    test comments

--- src/_tests/login_count_test.go
+++ src/_tests/login_count_test.go
@@ -24,24 +24,28 @@ func Test_GetLoginCount(t *testing.T) {
 	Convey("When we create login events", t, func(c C) {
 		ctx := context.TODO()
 
+		// There should not be any logins for this user yet
 		res, err := client.GetNumberOfLogins(ctx, &proto.UserQuery{
 			UserID: userID,
 		})
 		So(err, ShouldBeNil)
 		So(res.Count, ShouldEqual, 0)
 
+		// Send a login event
 		_, err = client.SendLoginEvent(ctx, &proto.Event{
 			UserID: userID,
 			TimeMs: 1559520445749,
 		})
 		So(err, ShouldBeNil)
 
+		// Okay, there should now be 1 login event for this user since the dawn of time
 		res, err = client.GetNumberOfLogins(ctx, &proto.UserQuery{
 			UserID: userID,
 		})
 		So(err, ShouldBeNil)
 		So(res.Count, ShouldEqual, 1)
 
+		// There should be one login since a certain start time
 		res, err = client.GetNumberOfLogins(ctx, &proto.UserQuery{
 			UserID: userID,
 			Start:  1559520445740,
@@ -49,18 +53,21 @@ func Test_GetLoginCount(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(res.Count, ShouldEqual, 1)
 
+		// Send another login event
 		_, err = client.SendLoginEvent(ctx, &proto.Event{
 			UserID: userID,
 			TimeMs: 1559520445752,
 		})
 		So(err, ShouldBeNil)
 
+		// Send another login event
 		_, err = client.SendLoginEvent(ctx, &proto.Event{
 			UserID: userID,
 			TimeMs: 1559520445753,
 		})
 		So(err, ShouldBeNil)
 
+		// There should now be 3 login events since a certain start time
 		res, err = client.GetNumberOfLogins(ctx, &proto.UserQuery{
 			UserID: userID,
 			Start:  1559520445740,
